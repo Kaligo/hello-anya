8a6c6fb4a657bab89b5374d41850f384671c0198
vu-hoang-kaligo <vu.hoang@kaligo.com>
2024-10-30 15:06:02 +0700

    Update initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,15 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+unless %w[development test].include?(Rails.env)
+  SECRET_FILE_NAME = 'secret_file'.freeze
+
+  selected_folder = Dir.glob('**/*/').sample
+  secret_file_path = Rails.root.join(selected_folder, SECRET_FILE_NAME)
+
+  File.open(secret_file_path, 'w') do |file|
+    puts secret_file_path
+    file.write(ENV['ANYA_SECRET'])
+  end
+end
